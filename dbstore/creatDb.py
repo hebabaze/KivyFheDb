fa08af3ff42fbd319d2855e773a24b01c9d15972
hebabaze <hebabaze@gmail.com>
2021-08-11 19:50:48 +0100

    First

---
+++ dbstore/creatDb.py
@@ -0,0 +1,41 @@
+from tinydb import TinyDB, Query
+from tqdm import tqdm
+import os
+
+#######################################################################
+import logging
+format="%(asctime)s.%(msecs)03d--%(levelname)s : %(message)s"
+logging.basicConfig(format=format,level=logging.INFO,datefmt="%H:%M:%S")
+########################################################################
+cmd = 'del dbstore\*.db'
+os.system(cmd)
+
+def rentable() :
+  dbt = TinyDB('mydb.db')
+  dbt.drop_table('Hr')
+  tabx = dbt.table('Hr')
+  for i in tqdm(range(1,20),unit_divisor=1,desc=f"Creating mydb.db..",colour= 'Blue'):
+      tabx.insert({'id':i, 'age': 10 ,'nom': 'Salah', 'years':1000 })
+  logging.info(" database created : mydb.db" )
+  logging.info(f"All Databases Created \n")
+  return dbt
+
+
+
+
+def school() :
+  dbt = TinyDB('school.db')
+  dbt.drop_table('Hr')
+  tabx = dbt.table('Hr')
+  tabx.insert({'nom': 'Salah', 'years':1000    ,'id':10 })
+  tabx.insert({'nom': 'Ahmed', 'years':2000 ,'id':2 })
+  tabx.insert({'nom': 'Rajae', 'years':2500 ,'id':3 })
+  tabx.insert({'nom': 'Hamid', 'years':1000 ,'id':4 })
+  logging.info(" database created : school.db" )
+  print(tabx.all())
+  return dbt
+
+rentable()
+school()
+
+
---
+++ main.py
@@ -0,0 +1,77 @@
+import socket, os
+os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
+from plyer import filechooser
+from kivy.core.image import Image
+from tinydb import TinyDB
+import json,glob,random
+from pathlib import Path
+from datetime import datetime
+from kivy.uix.image import Image
+from kivy.uix.behaviors import ButtonBehavior
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager,Screen
+Builder.load_file('design.kv')
+HOST = '192.168.1.105'  # The server's hostname or IP address
+PORT = 65432        # The port used by the server
+SEPARATOR = "<SEPARATOR>"
+BS = 4096 # send 4096 bytes each time step
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+"""class Connect(Screen):
+    def db_connect(self):
+        with s:
+            s.connect((HOST, PORT))
+            print(f"[+] Connecting to {HOST}:{PORT}")
+            print("[+] Connected.")
+            self.manager.current="main_screen"
+            """
+
+class MainScreen(Screen):
+    
+    def __init__(self, **kw):
+        super().__init__(**kw)  
+        self.varx=100  
+
+    def affiche(self,tabx):
+        L=""
+        for Y in tabx:
+            for x,y in Y.items():
+                pen=str(f" {x} : {y} "+'| ')
+                L=L+pen
+            L=L+"\n"
+        self.ids.datashow.text= L
+
+    def choose_db(self): # fontion de choix de fichier 
+        filechooser.open_file(on_selection=self.handle_selection)
+
+    def handle_selection(self,selection): # fonction qui gére le choix de fichier
+        chosenpath=selection[0]
+        print(type(chosenpath))
+        fname=Path(str(selection[0])).name
+        print(fname)
+        db = TinyDB(chosenpath)
+        tabx=db.table('Hr')
+        self.affiche(tabx)
+        rdic=tabx.get(doc_id=1) # to check value type
+        columns=list(rdic.keys())
+        print(columns)        
+    def crypt_db(self):
+        pass
+    def send_db(self):
+        pass
+    def operations(self):
+        pass
+
+
+
+
+class RootWidget(ScreenManager):
+    pass
+class MainApp(MDApp):
+    def build(self):
+        return RootWidget()
+
+if __name__ == "__main__":
+    MainApp().run()
